1053745f0b8c88e97bc8f8f5b5f3288b902bc25e
Evan Denerley <EvanDenerley@gmail.com>
2015-11-19 14:24:15 -0500

    squash bugs that crash heroku server api [fd60267] modifying response error [9d9e607] add typeof to logic statements [7738fd1] add dependency [1b0d79f] remove dead code [c9adfad] add dependency [703248e] params and typo [529bb8b] add error handling

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
+var rollbar = require('rollbar');
+var rollbarKey = "57fcf7c2515b4dd3916052ed09902fe8";
+
+//initiate rollbar logging
+rollbar.init(rollbarKey);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Use the rollbar error handler to send exceptions to your rollbar account
+app.use(rollbar.errorHandler(rollbarKey));
+
+// catch and forward
+app.use(function (req, res, next) {
+  logError(err, next);
 });
 
 // error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+  app.use(function (err, req, res, next) {
+    logError(err, next);
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+if (app.get('env') === 'production') {
+  app.use(function (err, req, res, next) {
+    logError(err, next);
   });
-});
+}
 
+function logError(err, next) {
+  rollbar.reportMessage(err);
+  next(err);
+}
 
 module.exports = app;
