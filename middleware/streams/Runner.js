ededa6a3e5b933da833b4b75c9ac12c6bd2985b2
Chris Coffey <cmcof273@gmail.com>
2016-01-08 19:32:29 -0500

    Stream [c22112e]terminate statements [25bf078] poc of child process with list of id passing [c75d1d8] clean up app codes and begin work on Steam [81cafa1] add mongoose mongo client [eda6bd5] update gitignore

---
+++ middleware/streams/Runner.js
@@ -0,0 +1,19 @@
+const
+    S = require('../core/appStates.js');
+    fs = require('fs');
+    cp = require('child_process');
+    _ = require('underscore');
+
+//This would be triggered by a timer
+var launchLoop = function(ls, i){
+    var p = cp.fork("./middleware/streams/StreamTest.js", [ls]);
+
+    p.on(S.close, function(exitCode){
+        console.log("ran process "+ i + "happily. Exited with code "+ exitCode);
+    });
+};
+
+var x = _.range(10);
+_.each(x, function(n, i, ls){
+    launchLoop(ls, i);
+});
