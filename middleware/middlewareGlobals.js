b40961b59612ccd8402c07abc5dc3db696efd612
Evan Denerley <EvanDenerley@gmail.com>
2016-01-20 15:05:00 -0500

    comments organization

--- middleware/middlewareGlobals.js
+++ middleware/middlewareGlobals.js
@@ -1,8 +1,10 @@
 "use strict";
-exports.GOOGLE_PLUS_PEOPLE_URL = "https://www.googleapis.com/plus/v1/people/";
-exports.FEATURED_URL = "/featured";
-exports.MESSAGES_URL = "/messages";
-exports.SEARCH_URL = "/search";
+/**
+ * Common Firebase middleware path urls.
+ * @type {string}
+ */
+exports.ROOT_URL = "/";
+exports.USERS_URL = "/users";
 exports.USER_URL = "/user";
 exports.USER_CHANNELS_URL = "/user/channels";
 exports.USER_CONTACTS_URL = "/user/contacts";
@@ -10,10 +12,16 @@ exports.USER_FOLLOWER_COUNT_URL = "/user/following";
 exports.USER_GROUPS_URL = "/user/groups";
 exports.USER_SHARED_URL = "/user/shared";
 exports.USER_VERSION_URL = "/user/version";
-exports.ROOT_URL = "/";
-exports.USERS_URL = "/users";
 exports.WEBUSERS_URL = "/webusers";
+exports.FEATURED_URL = "/featured";
+exports.MESSAGES_URL = "/messages";
+exports.SEARCH_URL = "/search";
+exports.GOOGLE_PLUS_PEOPLE_URL = "https://www.googleapis.com/plus/v1/people/";
 exports.GOOGLE_PERSON_URL = "/google/person";
+/**
+ * Common Firebase middleware data strings.
+ * @type {string}
+ */
 exports.USERS = "users";
 exports.USER = "user";
 exports.USERID = "userId";
@@ -30,8 +38,11 @@ exports.FEATURED = "featured";
 exports.VALUE = "value";
 exports.NAME = "name";
 exports.AUTH = "auth";
-exports.UNDEFINED = 'undefined';
-exports.ERROR = "error";
 exports.END = "end";
 exports.DATA = "data";
-exports.PARAM_KEY = "?key=";
+/**
+ * Common middlware error strings.
+ * @type {string}
+ */
+exports.ERROR = "error";
+exports.UNDEFINED = 'undefined';
