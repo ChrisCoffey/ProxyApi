da014f74e0ea23babd597885932fe245a7986bc5
Chris Coffey <cmcof273@gmail.com>
2016-01-27 20:58:10 -0500

    beginnings of a subscription system

--- middleware/core/subscriptions.js
+++ middleware/core/subscriptions.js
@@ -1,12 +1,33 @@
+const 
+    firebaseClient = require('firebase'),
+    scraperBrain = require('scraperBrain'),
+    _ = require('underscore');
+
+var activeSubscriptions = [];
+
 var Subscription = function(userId){
     this.userId = userId;
 };
 
-Subscription.prototype.cancel = function(){
+Subscription.prototype.cancel = function() {
+    
 };
 
+Subscription.prototype.start = function() {
 
-exports.subscribe = function()
+    var userChannels = firebaseClient.getChannelsForUser(this.userId);
+    var pairs =  _.map(userChannels, function(chan){
+        return {userId: this.userId, channelName: chan.name};
+    });
+    _.each(pairs, function(pair){
+        scraperBrain.registerUser(pair);
+    });
+
+    activeSubscriptions.push(this);
+};
+
+exports.subscribe = function(userId){ return new Subscription(userId) };
+
 
 //client calls subscribe()
 //server creates a subscription
